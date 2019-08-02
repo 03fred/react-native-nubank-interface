1f592e4603f7c528efca9eae3ca543bcb04f119e
03fred <fred.rocke02@gmail.com>
2019-08-02 10:36:38 -0300

    interface nubank

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import Header from '~/components/Header';
-import { Container } from './styles';
+import Tabs from '~/components/Tabs';
+
+import { Container,Content,Card,CardHeader,CardContent,Title,CardFooter,Description,Annotation } from './styles';
 
 
 export default function Main() {
@@ -9,6 +11,26 @@ export default function Main() {
     return ( 
       <Container> 
        <Header/>
+      <Content>      
+        <Card>
+        <CardHeader>
+        <Icon name="attach-money" size={28} color="#666"/>
+        <Icon name="visibility-off" size={28} color="#666"/>
+        </CardHeader>
+
+        <CardContent>
+        <Title>Saldo Disponível</Title>
+        <Description>R$ 7.000,00</Description>
+        </CardContent>
+
+        <CardFooter>
+         <Annotation>
+              Transferência realizada com sucesso.
+         </Annotation>
+        </CardFooter>
+        </Card>
+      </Content>
+      <Tabs/>
       </Container>
     );
 }
